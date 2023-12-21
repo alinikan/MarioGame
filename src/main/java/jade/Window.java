0e6e0fad44d4b2e510edf7538f95f5f2986a3b97
Ali Nikan <ana106@sfu.ca>
2023-12-21 14:27:47 -0800

    changed screen to white

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -98,7 +98,7 @@ public class Window {
             glfwPollEvents();
 
             // Set the clear color and clear the framebuffer.
-            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
 
             // Swap the color buffers.
